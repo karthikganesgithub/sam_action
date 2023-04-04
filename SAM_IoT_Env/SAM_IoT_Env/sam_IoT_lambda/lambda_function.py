e712208b6247049c4379bb82f787c53c5d035c4b
Karthik <118618656+karthikganesgithub@users.noreply.github.com>
2023-04-03 17:35:11 -0700

    Add files via upload

---
+++ SAM_IoT_Env/SAM_IoT_Env/sam_IoT_lambda/lambda_function.py
@@ -0,0 +1,42 @@
+import pymysql
+import json
+import time
+import os
+import boto3
+
+client = boto3.client('secretsmanager')
+
+environment = os.environ['ENVIRONMENT']
+
+# Construct the table name based on the environment variable
+# table_name = f'{environment}_cpu_util'
+
+#Estabilish connection
+#connection = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name)
+
+def lambda_handler(event, context):
+    
+    # cpu_usage = event['CPU Usage']
+    # timestamp = event['Timestamp']
+    # print("CPU Usage:", cpu_usage)
+    # print("Timestamp:", timestamp)
+    # sql = INSERT INTO CPU_Util (CPU_Utilization, Time_stamp) VALUES (%s, %s)
+    # values = (cpu_usage, timestamp)
+    # cursor.execute(sql, values)
+    response = client.get_secret_value(SecretId = f'{environment}/rds/key')
+    secret_string = response['SecretString']
+    # print(secret_string)
+    secrets = json.loads(secret_string)
+    endpoint = secrets['host']
+    username = secrets['username']
+    password = secrets['password']
+    database = secrets['dbname']
+    # print(database)
+    connection = pymysql.connect(host=endpoint, user=username, passwd=password, db=database)
+    cursor = connection.cursor()
+    cursor.execute("""INSERT INTO cpu_util (CPU_Utilization, Time_stamp) VALUES (%s, '%s')""" % (event['CPU Usage'], event['Timestamp']))
+    connection.commit()
+    connection.close()
+    return {
+        'statusCode': 200
+    }  
---
+++ SAM_IoT_Env/SAM_IoT_Env/y
@@ -0,0 +1,38 @@
+version = 0.1
+[dev]
+[dev.deploy]
+[dev.deploy.parameters]
+stack_name = "sam-app-dev"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-1rqnxsr0m1td6"
+s3_prefix = "sam-app-dev"
+region = "us-west-1"
+confirm_changeset = true
+capabilities = "CAPABILITY_IAM"
+disable_rollback = true
+parameter_overrides = "MyEnvironment=\"dev\""
+image_repositories = []
+
+[test]
+[test.deploy]
+[test.deploy.parameters]
+stack_name = "sam-app-dev"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-1rqnxsr0m1td6"
+s3_prefix = "sam-app-dev"
+region = "us-west-1"
+capabilities = "CAPABILITY_IAM"
+disable_rollback = true
+parameter_overrides = "MyEnvironment=\"test\""
+image_repositories = []
+
+[y]
+[y.deploy]
+[y.deploy.parameters]
+stack_name = "sam-app-dev"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-1rqnxsr0m1td6"
+s3_prefix = "sam-app-dev"
+region = "us-west-1"
+confirm_changeset = true
+capabilities = "CAPABILITY_IAM"
+disable_rollback = true
+parameter_overrides = "MyEnvironment=\"dev\""
+image_repositories = []
